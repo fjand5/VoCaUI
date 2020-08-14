14e7c75cab75aacf8b83253292985e0939168ff0
fjand5 <huynhtheluat@gmail.com>
2020-08-14 22:42:49 +0700

    Delete sysDefine.h

--- example/sysDefine.h
+++
@@ -1,9 +0,0 @@
-#ifndef SYSDEFINE
-#define SYSDEFINE
-#define NAME_DEVICE "TESTUI" // đặt tên cho dự án
-#define LED_PIN -1
-#define CONFIG_FILE F("/config.json")
-#define SIZE_CONFIG_FILE 8192
-#define LOG(l) Serial.println(l)
- // #define LOG(l)
-#endif
