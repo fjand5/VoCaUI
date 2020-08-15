d346c2ef959e776a9b1bd13ba3183524de44458e
fjand5 <huynhtheluat@gmail.com>
2020-08-15 18:23:55 +0700

    Update sysDefine.h

--- sysDefine.h
+++ sysDefine.h
@@ -1,6 +1,8 @@
 #ifndef SYSDEFINE
 #define SYSDEFINE
+#ifndef NAME_DEVICE
 #define NAME_DEVICE "TESTUI" // đặt tên cho dự án
+#endif
 #define LED_PIN -1
 #define CONFIG_FILE F("/config.json")
 #define SIZE_CONFIG_FILE 8192
